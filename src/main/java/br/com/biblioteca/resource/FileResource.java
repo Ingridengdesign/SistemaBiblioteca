1f88021a58e6b8f510214806c30459c2e606568b
Ingrid Emanuelle Sousa Dias <ingrid.dias@nav.com.br>
2022-04-29 14:56:51 -0300

    get e post funcionndo

--- src/main/java/br/com/biblioteca/resource/FileResource.java
+++ src/main/java/br/com/biblioteca/resource/FileResource.java
@@ -1,12 +1,17 @@
 package br.com.biblioteca.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -34,5 +39,14 @@ public class FileResource {
 		}catch(Exception e) {
 			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("O upload não foi possível");
 		}
+	}
+	
+	@GetMapping("/files/{filename:.+}")
+	@ResponseBody
+	public ResponseEntity<Resource> getFile(@PathVariable String filename){
+		Resource file = storageService.load(filename);
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+				.body(file);
 	}
 }
--- src/main/java/br/com/biblioteca/service/FileStorageService.java
+++ src/main/java/br/com/biblioteca/service/FileStorageService.java
@@ -1,9 +1,12 @@
 package br.com.biblioteca.service;
 
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,6 +20,21 @@ public class FileStorageService {
 			Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()));
 		}catch(Exception e) {
 			throw new RuntimeException("Não foi possível gravar o arquivo! " + e.getMessage());
+		}
+	}
+	
+	public Resource load(String filename) {
+		try {
+			Path file = root.resolve(filename);
+			Resource resource = new UrlResource(file.toUri());
+			
+			if(resource.exists() || resource.isReadable()) {
+				return resource;
+			}else {
+				throw new RuntimeException("Arquivo não pode ser lido");
+			}
+		}catch(MalformedURLException e) {
+			throw new RuntimeException("Error: " + e.getMessage());
 		}
 	}
 }
